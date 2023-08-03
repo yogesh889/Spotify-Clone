ad5a81fc2ce834f7a3214b043a87931c00198015
yogesh990 <8058yogesh@gmail.com>
2023-08-03 13:00:54 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,16 @@
+const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2cbbfd60c1msh2c94ce4586b2753p1131f9jsned1d572bdb53',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
