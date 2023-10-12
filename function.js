d290fbaae9579b168876b592dff3fad71c72ca86
Yogesh889 <8058yogesh@gmail.com>
2023-10-12 22:50:49 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,6 @@
+/**
+ * Logs a welcome message to the console.
+ */
 console.log("Welcome to Spotify");
 
 // Initialize the Variables
@@ -10,140 +13,150 @@ let masterSongName = document.getElementById("masterSongName");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
-  {
-    songName: "Warriyo - Mortals [NCS Release]",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
-  },
-  {
-    songName: "Cielo - Huma-Huma",
-    filePath: "songs/2.mp3",
-    coverPath: "covers/2.jpg",
-  },
-  {
-    songName: "DEAF KEV - Invincible [NCS Release]-320k",
-    filePath: "songs/3.mp3",
-    coverPath: "covers/3.jpg",
-  },
-  {
-    songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
-    filePath: "songs/4.mp3",
-    coverPath: "covers/4.jpg",
-  },
-  {
-    songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
-    filePath: "songs/5.mp3",
-    coverPath: "covers/5.jpg",
-  },
-  {
-    songName: "Rabba - Salam-e-Ishq",
-    filePath: "songs/2.mp3",
-    coverPath: "covers/6.jpg",
-  },
-  {
-    songName: "Sakhiyaan - Salam-e-Ishq",
-    filePath: "songs/2.mp3",
-    coverPath: "covers/7.jpg",
-  },
-  {
-    songName: "Bhula Dena - Salam-e-Ishq",
-    filePath: "songs/2.mp3",
-    coverPath: "covers/8.jpg",
-  },
-  {
-    songName: "Tumhari Kasam - Salam-e-Ishq",
-    filePath: "songs/2.mp3",
-    coverPath: "covers/9.jpg",
-  },
-  {
-    songName: "Na Jaana - Salam-e-Ishq",
-    filePath: "songs/4.mp3",
-    coverPath: "covers/10.jpg",
-  },
+	{
+		songName: "Warriyo - Mortals [NCS Release]",
+		filePath: "songs/1.mp3",
+		coverPath: "covers/1.jpg",
+	},
+	{
+		songName: "Cielo - Huma-Huma",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/2.jpg",
+	},
+	{
+		songName: "DEAF KEV - Invincible [NCS Release]-320k",
+		filePath: "songs/3.mp3",
+		coverPath: "covers/3.jpg",
+	},
+	{
+		songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
+		filePath: "songs/4.mp3",
+		coverPath: "covers/4.jpg",
+	},
+	{
+		songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+		filePath: "songs/5.mp3",
+		coverPath: "covers/5.jpg",
+	},
+	{
+		songName: "Rabba - Salam-e-Ishq",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/6.jpg",
+	},
+	{
+		songName: "Sakhiyaan - Salam-e-Ishq",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/7.jpg",
+	},
+	{
+		songName: "Bhula Dena - Salam-e-Ishq",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/8.jpg",
+	},
+	{
+		songName: "Tumhari Kasam - Salam-e-Ishq",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/9.jpg",
+	},
+	{
+		songName: "Na Jaana - Salam-e-Ishq",
+		filePath: "songs/4.mp3",
+		coverPath: "covers/10.jpg",
+	},
 ];
 
 songItems.forEach((element, i) => {
-  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
-  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+	element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+	element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 });
 
-// Handle play/pause click
+/**
+ * Handles the click event for the play/pause button.
+ */
 masterPlay.addEventListener("click", () => {
-  if (audioElement.paused || audioElement.currentTime <= 0) {
-    audioElement.play();
-    masterPlay.classList.remove("fa-play-circle");
-    masterPlay.classList.add("fa-pause-circle");
-    gif.style.opacity = 1;
-  } else {
-    audioElement.pause();
-    masterPlay.classList.remove("fa-pause-circle");
-    masterPlay.classList.add("fa-play-circle");
-    gif.style.opacity = 0;
-  }
+	if (audioElement.paused || audioElement.currentTime <= 0) {
+		audioElement.play();
+		masterPlay.classList.remove("fa-play-circle");
+		masterPlay.classList.add("fa-pause-circle");
+		gif.style.opacity = 1;
+	} else {
+		audioElement.pause();
+		masterPlay.classList.remove("fa-pause-circle");
+		masterPlay.classList.add("fa-play-circle");
+		gif.style.opacity = 0;
+	}
 });
+
 // Listen to Events
 audioElement.addEventListener("timeupdate", () => {
-  // Update Seekbar
-  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-  myProgressBar.value = progress;
+	// Update Seekbar
+	progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+	myProgressBar.value = progress;
 });
 
 myProgressBar.addEventListener("change", () => {
-  audioElement.currentTime =
-    (myProgressBar.value * audioElement.duration) / 100;
+	audioElement.currentTime =
+		(myProgressBar.value * audioElement.duration) / 100;
 });
 
+/**
+ * Changes the play icon to pause for all song items.
+ */
 const makeAllPlays = () => {
-  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
-    (element) => {
-      element.classList.remove("fa-pause-circle");
-      element.classList.add("fa-play-circle");
-    }
-  );
+	Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+		(element) => {
+			element.classList.remove("fa-pause-circle");
+			element.classList.add("fa-play-circle");
+		}
+	);
 };
 
 Array.from(document.getElementsByClassName("songItemPlay")).forEach(
-  (element) => {
-    element.addEventListener("click", (e) => {
-      makeAllPlays();
-      songIndex = parseInt(e.target.id);
-      e.target.classList.remove("fa-play-circle");
-      e.target.classList.add("fa-pause-circle");
-      audioElement.src = `songs/${songIndex + 1}.mp3`;
-      masterSongName.innerText = songs[songIndex].songName;
-      audioElement.currentTime = 0;
-      audioElement.play();
-      gif.style.opacity = 1;
-      masterPlay.classList.remove("fa-play-circle");
-      masterPlay.classList.add("fa-pause-circle");
-    });
-  }
+	(element) => {
+		/**
+		 * Handles the click event for a song item.
+		 * @param {Event} e - The click event.
+		 */
+		element.addEventListener("click", (e) => {
+			makeAllPlays();
+			songIndex = parseInt(e.target.id);
+			e.target.classList.remove("fa-play-circle");
+			e.target.classList.add("fa-pause-circle");
+			audioElement.src = `songs/${songIndex + 1}.mp3`;
+			masterSongName.innerText = songs[songIndex].songName;
+			audioElement.currentTime = 0;
+			audioElement.play();
+			gif.style.opacity = 1;
+			masterPlay.classList.remove("fa-play-circle");
+			masterPlay.classList.add("fa-pause-circle");
+		});
+	}
 );
 
 document.getElementById("next").addEventListener("click", () => {
-  if (songIndex >= 9) {
-    songIndex = 0;
-  } else {
-    songIndex += 1;
-  }
-  audioElement.src = `songs/${songIndex + 1}.mp3`;
-  masterSongName.innerText = songs[songIndex].songName;
-  audioElement.currentTime = 0;
-  audioElement.play();
-  masterPlay.classList.remove("fa-play-circle");
-  masterPlay.classList.add("fa-pause-circle");
+	if (songIndex >= 9) {
+		songIndex = 0;
+	} else {
+		songIndex += 1;
+	}
+	audioElement.src = `songs/${songIndex + 1}.mp3`;
+	masterSongName.innerText = songs[songIndex].songName;
+	audioElement.currentTime = 0;
+	audioElement.play();
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
 });
 
 document.getElementById("previous").addEventListener("click", () => {
-  if (songIndex <= 0) {
-    songIndex = 0;
-  } else {
-    songIndex -= 1;
-  }
-  audioElement.src = `songs/${songIndex + 1}.mp3`;
-  masterSongName.innerText = songs[songIndex].songName;
-  audioElement.currentTime = 0;
-  audioElement.play();
-  masterPlay.classList.remove("fa-play-circle");
-  masterPlay.classList.add("fa-pause-circle");
+	if (songIndex <= 0) {
+		songIndex = 0;
+	} else {
+		songIndex -= 1;
+	}
+	audioElement.src = `songs/${songIndex + 1}.mp3`;
+	masterSongName.innerText = songs[songIndex].songName;
+	audioElement.currentTime = 0;
+	audioElement.play();
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
 });
